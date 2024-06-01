3c77b7745722e84c568aafbe78f8516bbfd4c914
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-06-01 16:20:53 -0300

    finish-base-api

--- slim/index.php
+++ slim/index.php
@@ -19,6 +19,7 @@ $app = new \Slim\App([
 ]);
 
 $container = $app->getContainer();
+
 $container['db'] = function(){
 
     $capsule = new Capsule;
@@ -39,16 +40,53 @@ $container['db'] = function(){
     return $capsule;
 };
 
+
 $app->get('/usuarios', function(Request $request, Response $response) {
+
+    /*** Criando uma tabela pelo o Slim Framework 
     $db = $this->get('db')->schema(); // faltando o c
-    $db->dropIfExists('usuarios');
-    $db->create('usuarios', function($table){
+    $db->dropIfExists('usuarios2');
+    $db->create('usuarios2', function($table){
         $table->increments('id');
         $table->string('nome');
         $table->string('email');
         $table->string('senha');
         $table->timestamps();
     });
+    */
+    
+$db = $this->get('db');
+// Inserindo registros
+    // $db->table('usuarios')->insert([
+    //     'nome' => 'Beto Almeida',
+    //     'email' => 'betoalmeida@terra.com',
+    //     'senha' => '12345678',
+    // ]);
+
+// atualizar
+    // $db->table('usuarios')
+    //     ->where('id', 1)
+    //     ->update([
+    //             'nome' => 'Maria Silva Sousa',
+    //             'email' => 'email@gmail.com',
+    //             'senha' => '12300123',
+    //     ]);
+
+// Delete
+    // $db->table('usuarios')
+    //     ->where('id', 1)
+    //     ->delete();
+
+// Listar
+    // $usuarios = $db->table('usuarios')->get();
+    //  echo "<h1>Listar usuarios</h1>";
+    //  foreach($usuarios as $usuario){
+    //     echo "Nome: " . $usuario->nome. '<br>';
+    //     echo "E-mail: " . $usuario->email. '<br>';
+    //     echo "Senha: " . $usuario->senha. '<br>';
+    //     echo '<hr>';
+    //  };
+
 });
 
 $app->run();
